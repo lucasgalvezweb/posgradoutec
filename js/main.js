a7c0cf62dfb6537139b5b035f5b9d5082cbd90cd
Lucas Galvez Chamorro <lgalvez@utec.edu.pe>
2020-02-11 14:30:47 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* One page nav */
+
+$('#navbarCollapse').onePageNav({
+	currentClass: 'active',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+});
